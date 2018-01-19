c1afaa03c4ea80b7980b343b42f910a345b3e4b1
shuvam bosana <shuvambosana0705@gmail.com>
2018-01-19 15:59:40 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,16 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    var url = tab.url;
-    // if (changeInfo.url !== undefined && info.status == "loading" && url !== null) {
-    	chrome.tabs.sendMessage(tab.id, "Changed");
-    // }
-});
-chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 //     var url = tab.url;
-//     if (info.status == "complete" && url !== null) {
-    	chrome.tabs.sendMessage(tab.id, "Changed");
-//     }
-});
+//     // if (changeInfo.url !== undefined && info.status == "loading" && url !== null) {
+//     	chrome.tabs.sendMessage(tab.id, "Changed");
+//     // }
+// });
+// chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
+// //     var url = tab.url;
+// //     if (info.status == "complete" && url !== null) {
+//     	chrome.tabs.sendMessage(tab.id, "Changed");
+// //     }
+// });
+chrome.runtime.onMessage.addListener(function(response,resolve,send){
+	var url = response;
+	// window.open(url);
+})
