0d55177e00a5c10c6ccb188cc2dc90b716574b51
shuvam bosana <shuvambosana0705@gmail.com>
2018-01-19 15:59:04 +0530

    Update content.js

--- content.js
+++ content.js
@@ -41,21 +41,67 @@
 // 	});
 // });
 
-aync await function openNewBackgroundTab(downUrl){
-    var a = document.createElement("a");
-    a.href = downUrl;
-    var evt = document.createEvent("MouseEvents");
-    //the tenth parameter of initMouseEvent sets ctrl key
-    evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
-                                true, false, false, false, 0, null);
-    a.dispatchEvent(evt);
+// function openNewBackgroundTab(downUrl){
+//     var a = document.createElement("a");
+//     a.href = downUrl;
+//     var evt = document.createEvent("MouseEvents");
+//     //the tenth parameter of initMouseEvent sets ctrl key
+//     evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
+//                                 true, false, false, false, 0, null);
+//     a.dispatchEvent(evt);
 
+// }
+function createIframe(url){
+	console.log("dsg")
+	//document.body.appendChild("<div id='hiddenDownloader'></div>");
+	 var iframe;
+	   iframe = document.getElementById("hiddenDownloader");
+	   if (iframe == null) {
+	       iframe = document.createElement('iframe');
+	       iframe.id = "hiddenDownloader";
+	       iframe.style.visibility = 'hidden';
+	       document.body.appendChild(iframe);
+	   }
+	   iframe.src = url;
+	// var iframe = '<iframe class = "save-form-dlink" style="width:0;height:0;border:0; border:none;" src='+url+'></iframe>';
+	// $(document.body).append(iframe);
+}
+function createDiv(res){
+	var div= document.createElement("div");
+	div.id= "save-form-dLink"
+	document.body.appendChild(div)
+	$("#save-form-dLink").html(res);
+	
+}
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  var self =this;
+  $(win.document).ready(function(){
+  	setTimeout(function(){
+  		console.log("faf")
+  		console.log(self.win)
+  		// console.log($(win.document).contents().find("'#sf_url").html())
+  	},5000)
+  	
+  })
 }
 
 var url = window.location.href;
-var downUrl = url.substr(0,pos)+"ss"+url.substr(pos);
-window.open(downUrl)
-chrome.tabs.create({ url: downUrl });
+var pos = url.search("youtube");
+var saveFormUrl = url.substr(0,pos)+"ss"+url.substr(pos);
+// window.open(downUrl)
+console.log(saveFormUrl);
+openInNewTab(saveFormUrl);
+// createIframe(saveFormUrl);
+// $.ajax({
+//   url: saveFormUrl,
+//   dataType: "html",
+//   context: document.body,
+//   success : function(res){
+//   	createDiv(res)}
+// })
+// chrome.runtime.sendMessage(downUrl);
 // openNewBackgroundTab(downUrl)
 
 
